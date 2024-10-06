3c997a0906b3fb8cf4997deb3ecf64d092bdbbaf
Prem Modha <premmodhaofficial@gmail.com>
2024-10-06 16:22:39 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::fs;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -21,15 +20,96 @@ fn main() {
                 String::new()
             });
 
-            // Uncomment this block to pass the first stage
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                tokenize(file_contents)
             } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+                println!("EOF  null");
             }
         }
         _ => {
             eprintln!("Unknown command: {}", command);
         }
     }
+}
+
+fn tokenize(file_contents: String) {
+    for token in file_contents.split(" ") {
+        for ch in token.chars() {
+            match ch {
+                '(' => println!("{} {} {}", "LEFT_PAREN", ch, "null"),
+                ')' => println!("{} {} {}", "RIGHT_PAREN", ch, "null"),
+                _ => continue,
+            }
+        }
+    }
+    println!("EOF  null");
+}
+
+#[derive()]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    litral: String,
+    line: i32,
+}
+
+impl Token {
+    fn new(token_type: TokenType, lexeme: String, litral: String, line: i32) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            litral,
+            line,
+        }
+    }
+}
+#[derive(std::fmt::Debug)]
+enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // one or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // literals.
+    Identifier,
+    String,
+    Number,
+
+    // keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
 }
